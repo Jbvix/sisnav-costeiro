a4bfded22463615e88ce2484e1a3ff1e7dfc389e
Jossian Costa <jossiancosta@gmail.com>
2026-01-13 10:01:50 -0300

    updateEnviroData

--- js/services/TideLocator.js
+++ js/services/TideLocator.js
@@ -20,17 +20,17 @@ const TideLocator = {
         { id: 'BR_SFS', name: 'São Francisco do Sul', lat: -26.233, lon: -48.633, csvName: 'São Francisco do Sul' },
         { id: 'BR_ITJ', name: 'Itajaí', lat: -26.916, lon: -48.650, csvName: 'Itajaí' },
         { id: 'BR_IMB', name: 'Imbituba', lat: -28.233, lon: -48.650, csvName: 'Imbituba' },
-        { id: 'BR_STS', name: 'Santos (Ponta da Praia)', lat: -23.960, lon: -46.310, csvName: 'Paranaguá' }, // Proxy: Paranaguá (Santos missing)
-        { id: 'BR_SSB', name: 'São Sebastião', lat: -23.816, lon: -45.400, csvName: 'Paranaguá' }, // Proxy: Paranaguá
+        { id: 'BR_STS', name: 'Santos (Ponta da Praia)', lat: -23.960, lon: -46.310, csvName: 'Santos' },
+        { id: 'BR_SSB', name: 'São Sebastião', lat: -23.816, lon: -45.400, csvName: 'São Sebastião' },
         { id: 'BR_RIO', name: 'Rio de Janeiro (Ilha Fiscal)', lat: -22.896, lon: -43.165, csvName: 'Rio de Janeiro' },
         { id: 'BR_SEP', name: 'Sepetiba', lat: -23.016, lon: -44.033, csvName: 'Sepetiba' },
         { id: 'BR_VIT', name: 'Vitória', lat: -20.316, lon: -40.283, csvName: 'Vitória' },
         { id: 'BR_SAL', name: 'Salvador', lat: -12.966, lon: -38.516, csvName: 'Salvador' },
         { id: 'BR_REC', name: 'Recife', lat: -8.050, lon: -34.866, csvName: 'Recife' },
         { id: 'BR_SUA', name: 'Suape', lat: -8.397, lon: -34.959, csvName: 'Suape' },
-        { id: 'BR_FOR', name: 'Fortaleza (Mucuripe)', lat: -3.716, lon: -38.466, csvName: 'Mucuripe' },
+        { id: 'BR_FOR', name: 'Fortaleza (Mucuripe)', lat: -3.716, lon: -38.466, csvName: 'Fortaleza' },
         { id: 'BR_BEL', name: 'Belém', lat: -1.450, lon: -48.500, csvName: 'Belém' },
-        { id: 'BR_VDC', 'name': 'Vila do Conde', lat: -1.533, lon: -48.750, csvName: 'Belém' }, // Proxy: Belém (Vila do Conde missing)
+        { id: 'BR_VDC', 'name': 'Vila do Conde', lat: -1.533, lon: -48.750, csvName: 'Vila do Conde' },
         { id: 'BR_ITQ', 'name': 'Itaqui', lat: -2.566, lon: -44.366, csvName: 'Itaqui' }
     ],
 
